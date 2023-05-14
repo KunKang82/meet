b188f7151acc198d066d6900ec24944de35bb7ab
KunKang82 <kunzsayz@hotmail.com>
2023-05-14 15:29:43 -0400

    task 4.3

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,7 +19,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://kunkang82.github.io/meet"],
+  redirect_uris: ["https://kunkang82.github.io/meet/"],
   javascript_origins: ["https://kunkang82.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
@@ -104,3 +104,55 @@ module.exports.getAccessToken = async (event) => {
         };
       });
   };
+
+  module.exports.getCalendarEvents = event => {
+
+    const oAuth2Client = new google.auth.OAuth2(
+      client_id,
+      client_secret,
+      redirect_uris[0]
+    );
+    // Decode authorization code extracted from the URL query
+    const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+    
+    oAuth2Client.setCredentials({ access_token });
+
+    return new Promise( (resolve, reject) => {
+
+      calendar.events.list(
+        {
+          calendarId: calendar_id,
+          auth: oAuth2Client,
+          timeMin: new Date().toISOString(),
+          singleEvents: true,
+          orderBy: "startTime",
+        },
+        (error, response) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(response);
+          }
+        }
+      );
+
+    })
+    .then( results => {
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ events: results.data.items })
+      };
+    })
+    .catch( error => {
+      return {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(error),
+      };
+    })
+  }
