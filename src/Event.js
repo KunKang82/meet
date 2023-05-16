e5b85a79cecfc68ae68bf13b3888825dcae321a8
KunKang82 <kunzsayz@hotmail.com>
2023-05-15 23:27:14 -0400

    4.4 Scenario 2

---
+++ src/Event.js
@@ -0,0 +1,20 @@
+// import React, { Component } from "react";
+
+// class Event extends Component {
+//   render() {
+//     return <div></div>;
+//   }
+// }
+// export default Event;
+
+
+// src/Event.js
+
+import React, { Component } from "react";
+
+class Event extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default Event;
---
+++ src/EventList.js
@@ -0,0 +1,40 @@
+// import React, { Component } from 'react';
+// import Event from './Event';
+
+// class EventList extends Component {
+//   render() {
+//     const { events } = this.props;
+//     return (
+//       <ul className="EventList">
+//         {events.map(event =>
+//           <li key={event.id}>
+//             <Event event={event} />
+//           </li>
+//         )}
+//       </ul>
+//     );
+//   }
+// }
+
+// export default EventList;
+
+
+import React, { Component } from 'react';
+import Event from './Event'; // in src/EventList.js
+
+class EventList extends Component {
+  render() {
+    const { events } = this.props;
+    return (
+      <ul className="EventList">
+        {events.map(event =>
+          <li key={event.id}>
+          <Event event={event} />
+        </li>
+        )}
+      </ul>
+    );
+  }
+}
+
+export default EventList;
