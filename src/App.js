35f5767476f1756f676b5ee39411953ad71a01ff
KunKang82 <kunzsayz@hotmail.com>
2023-05-15 23:46:57 -0400

    Updating EventList.test.js to use Mock Data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,3 @@
-// import React, { Component } from 'react';
-// import './App.css';
-// import EventList from './EventList';
-// import CitySearch from './CitySearch';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <CitySearch />
-//         <EventList />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-
 // src/App.js
 
 import React, { Component } from 'react';
--- src/Event.js
+++ src/Event.js
@@ -1,13 +1,3 @@
-// import React, { Component } from "react";
-
-// class Event extends Component {
-//   render() {
-//     return <div></div>;
-//   }
-// }
-// export default Event;
-
-
 // src/Event.js
 
 import React, { Component } from "react";
