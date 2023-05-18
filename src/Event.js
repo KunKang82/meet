20a18e24b74fe5b05e5ae99edeb3f8de3300ad61
KunKang82 <kunzsayz@hotmail.com>
2023-05-18 00:19:35 -0400

    4.4 final

--- src/Event.js
+++ src/Event.js
@@ -1,10 +1,43 @@
-// src/Event.js
+// // src/Event.js
 
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Event extends Component {
+  eventDetailsToggler = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  state = { show: false };
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+    return (
+      <div className="event-card">
+        <h5 className="event-title">{event.summary}</h5>
+        <p className="event-info">
+          {event.location} {event.start.dateTime} {event.start.timeZone}
+        </p>
+        {this.state.show && (
+          <p className="event-description">{event.description}</p>
+        )}
+        {!this.state.show ? (
+          <button
+            className="show_details-button"
+            onClick={this.eventDetailsToggler}
+          >
+            Show Details
+          </button>
+        ) : (
+          <button
+            className="hide_details-button"
+            onClick={this.eventDetailsToggler}
+          >
+            Hide Details
+          </button>
+        )}
+      </div>
+    );
   }
 }
+
 export default Event;
--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import CitySearch from '../CitySearch';
 import { mockData } from '../mock-data';
 import { extractLocations } from '../api';
+import CitySearch from '../CitySearch';
 
 describe('<CitySearch /> component', () => {
   let locations, CitySearchWrapper;
