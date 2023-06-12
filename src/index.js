6d61c7994e9ddb2d8c2771f500e010295cbc88bc
KunKang82 <kunzsayz@hotmail.com>
2023-06-12 00:43:14 -0400

    4.7

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('35a1338d2c6c462caede2e94a563de7c').install();
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -31,3 +32,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// atatus.notify(new Error('Test Atatus Setup')); //check if Atatus has been integrated successfully
